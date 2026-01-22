e6fc8825d56970cd7344fd99bd82e8c41da926d2
steeve laquitaine <laquitainesteeve@gmail.com>
2026-01-22 13:09:17 +0100

    implement unit-test pipeline

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+"""unit-tests
+
+Requirements:
+
+- 'data/20251219_PulsingGratings_PupilSize/Analysis/triggers/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
+
+"""
+import pytest
+import utils_drifting_gratings as utils
+
+
+def test_load_obj():
+    """test load_obj function
+    """
+    # size (152K)
+    test_data_path = 'data/20251219_PulsingGratings_PupilSize/Analysis/triggers/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
+    
+    # load data
+    output = utils.load_obj(test_data_path)
+
+    # test
+    assert isinstance(output, dict)
--- tests/tests.py
+++
@@ -1,5 +0,0 @@
-"""unit-tests
-"""
-
-def test_load_obj():
-    pass
