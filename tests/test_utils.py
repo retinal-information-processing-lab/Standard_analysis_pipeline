4c964c1e96196052e787592cfbf296cd7f6769e1
BaptisteLorenzi <lorenzibaptiste4@gmail.com>
2026-01-30 15:48:47 +0100

    Re-adding test_load_Objects

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,41 +24,40 @@ import numpy as np
 from unittest.mock import patch, MagicMock
 from io import StringIO
 
-# class TestLoadObj:
-    # """Test suite for load_obj function
-    # This regression test is only used in Step 1 because it's complicated to reproduce after (needs original dataset downloaded)
-    # 
-    # """
+class TestLoadObj:
+    """Test suite for load_obj function
     
-    # def test_load_obj_type(self):
-    #     """test output type with Pulsing Gratings 
-    #     known trigger data
-    #     """
-    #     # size (152K)
-    #     test_data_path = 'tests/test_data/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
+    """
+    
+    def test_load_obj_type(self):
+        """test output type with Pulsing Gratings 
+        known trigger data
+        """
+        # size (152K)
+        test_data_path = 'tests/test_data/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
         
-    #     # load data
-    #     output = utils.load_obj(test_data_path)
+        # load data
+        output = utils.load_obj(test_data_path)
 
-    #     # test
-    #     assert isinstance(output, dict)
+        # test
+        assert isinstance(output, dict)
 
 
-    # def test_load_obj_keys(self):
-    #     """test output keys with Pulsing Gratings 
-    #     known trigger data
-    #     """
-    #     # size (152K)
-    #     test_data_path = 'tests/test_data/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
+    def test_load_obj_keys(self):
+        """test output keys with Pulsing Gratings 
+        known trigger data
+        """
+        # size (152K)
+        test_data_path = 'tests/test_data/20251219_PulsingGratings_PupilSize_03_DG_50Hz_50%30ND_triggers.pkl'
 
-    #     # expected output keys
-    #     expected_keys = ['indices', 'duration', 'trigger_type', 'indice_errors']
+        # expected output keys
+        expected_keys = ['indices', 'duration', 'trigger_type', 'indice_errors']
         
-    #     # load data
-    #     output = utils.load_obj(test_data_path)
+        # load data
+        output = utils.load_obj(test_data_path)
 
-    #     # test
-    #     assert list(output.keys()) == expected_keys
+        # test
+        assert list(output.keys()) == expected_keys
 
 
 class TestSaveObj:
